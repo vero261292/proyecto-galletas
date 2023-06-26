547bbe84d6d82351e95d0b7b41dbaf2e3fac80c8
veronica perez <135690118+vero261292@users.noreply.github.com>
2023-06-26 07:22:52 -0600

    oi

--- vite-gallets/src/components/GalletSuerte.jsx
+++ vite-gallets/src/components/GalletSuerte.jsx
@@ -3,8 +3,19 @@
 const GalletSuerte = () => {
 
     return (
-        <article>
-            <i className='bx bx-envelope'></i>
+        <article className="galle-sue">
+            <h1>GALLETAS DE LA FORTUNA </h1>
+
+            <div className="contenedor">
+                
+
+
+            </div>
+           
+            
+           
+             
+            
         </article>
 
 
