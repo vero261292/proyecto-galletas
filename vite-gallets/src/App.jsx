62ac4fc103125c02e6d7c8bd36b64b590830e315
veronica perez <135690118+vero261292@users.noreply.github.com>
2023-06-27 07:40:29 -0600

    koi

--- vite-gallets/src/App.jsx
+++ vite-gallets/src/App.jsx
@@ -1,18 +1,36 @@
 
 
-import Contenedor from './components/Contenedor'
+import {useState} from 'react'
 import GalletSuerte from './components/GalletSuerte'
 import './App.css'
+import gallets from './beta/phrases.json'
 
 function App() {
- 
+  const fond = ['/fondos/fondo1.jpg', '/fondos/fondo2.jpg', '/fondos/fondo3.jpg', '/fondos/fondo4.jpg']
+  const [index, useIndex] = useState (0)
+  const [fondo, usefondo]= useState (0)
 
+  const suerte = () => {
+    if (fondo < fond.length -1) {
+      usefondo(fondo + 1)
+    }else{
+      usefondo(0)
+    }
+    
+    useIndex(Math.floor(Math.random()*gallets.length))
+  }
+  document.body.style= `background-image: url(${fond[fondo]})`
   return (
     <>
-     <GalletSuerte />
-     <Contenedor />
+     <GalletSuerte 
+     beta={gallets[index]}/>
+     <div>
+      <button onClick= {suerte}>ver suerte</button>
+     </div>
+     
     </>
   )
 }
 
 export default App
+
--- vite-gallets/src/components/GalletSuerte.jsx
+++ vite-gallets/src/components/GalletSuerte.jsx
@@ -1,16 +1,22 @@
 
 
-const GalletSuerte = () => {
+const GalletSuerte = (props) => {
+    const{beta}= props
 
     return (
-        <article className="galle-sue">
+        <article >
             <h1>GALLETAS DE LA FORTUNA </h1>
+            <div className="pras">
+                <p>
+                    {beta.phrase}
+                </p>
+            </div>
 
-            <p>
-                <div className="contenedor">
-                </div>
-            </p>
-            
+            <div className="aut">
+                <p>
+                    {beta.author}
+                </p>
+            </div>          
         </article>
 
 
@@ -18,5 +24,6 @@ const GalletSuerte = () => {
 }
 
 export default GalletSuerte
+
 
 
